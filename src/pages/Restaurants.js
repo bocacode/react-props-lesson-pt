0010bd27dea53ca3c731368a146cfa6475771895
jonathans199 <jso1919@gmail.com>
2022-08-15 21:13:25 -0400

    first commit

--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './../assets/styles.css'
 import SingleRestaurant from '../components/SingleRestaurant'
@@ -6,17 +6,21 @@ import SingleRestaurant from '../components/SingleRestaurant'
 const Restaurants = () => {
   const [allRestaurants, setAllRestaurants] = useState([])
 
-  const getData = () => {
-    fetch('https://bocacode-intranet-api.web.app/restaurants')
-      .then(raw => raw.json())
-      .then(data => setAllRestaurants(data))
-      .catch(err => console.error(err))
-  }
+  useEffect(() => {
+    const getData = () => {
+      fetch('https://bocacode-intranet-api.web.app/restaurants')
+        .then(raw => raw.json())
+        .then(data => setAllRestaurants(data))
+        .catch(err => console.error(err))
+    }
+
+    getData()
+  }, [])
 
   return (
     <div className='container'>
       <h1>Jons Restaurants</h1>
-      <button onClick={() => getData()}>Get restaurants</button>
+      {/* <button onClick={() => getData()}>Get restaurants</button> */}
       {allRestaurants.map(single => {
         // console.log(single)
         // return <SingleRestaurant key={single.id} name={single.name} address={single.address} photoUrl={single.photoUrl} />
